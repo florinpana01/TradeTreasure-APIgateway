b6662dc8bddda6d8eaaf6118690e1a5d9228bf1a
florinpana01 <florinpana98@gmail.com>
2023-11-06 12:36:39 +0100

    Product service connected to the API gateway

--- api-gateway/src/product/product.module.ts
+++ api-gateway/src/product/product.module.ts
@@ -1,7 +1,27 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
+import {Product} from './product.entity';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {ClientsModule, Transport} from '@nestjs/microservices';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Product]),
+    ProductModule,
+    ClientsModule.register([
+      {
+        name: 'PRODUCT_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqps://pjfufoya:LuO22_OIV_SIcvDewsRCbu_nTAUooYvt@kangaroo.rmq.cloudamqp.com/pjfufoya'],
+          queue: "products_queue",
+          queueOptions: {
+            durable: false
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [ProductController]
 })
 export class ProductModule {}
