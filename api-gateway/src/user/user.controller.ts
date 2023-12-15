f37c3cc879542ab30fda99610204ab714b914933
florinpana01 <florinpana98@gmail.com>
2023-12-15 14:44:46 +0100

    get user by email

--- api-gateway/src/user/user.controller.ts
+++ api-gateway/src/user/user.controller.ts
@@ -1,77 +1,81 @@
 import { Body, Controller, Delete, Get, Inject, Param, Post, Put } from '@nestjs/common';
 import { ClientProxy, EventPattern } from '@nestjs/microservices';
 import * as bcrypt from 'bcrypt';
-import {log} from 'console';
-
-
 
 @Controller('users')
 export class UserController {
-    constructor(
-        @Inject('USER_SERVICE') private readonly client: ClientProxy
-    ) {}
 
-    @Get()
-    async all() {
-        const result = await this.client.send('user_request_all', {});
-        return result;
-    }
+  constructor(
+    @Inject('USER_SERVICE') private readonly client: ClientProxy,
+  ) {}
 
-    @EventPattern('test')
-    async hello() {
-        console.log("  ");
-    }
+  @Get()
+  async all() {
+    const result = await this.client.send('user_request_all', {});
+    return result;
+  }
 
-    @EventPattern('product_created')
-    async helloNew(product: any){
-        console.log(product);
-        
-    }
+  @Get('email/:email')
+  async getUserByEmail(@Param('email') email: string) {
+    const result = await this.client.send('user_request_by_email', { email }).toPromise();
+    console.log(result);
+    return result;
+  }
 
-    @Post('register')
-    async register(
-        @Body('firstName') firstName: string,
-        @Body('lastName') lastName: string,
-        @Body('email') email: string,
-        @Body('password') password: string,
-        ) {
-            const hashedPassword = await bcrypt.hash(password, 12);
-            console.log("register");
-            
-            const result = this.client.send('user_created_gateway', {firstName, lastName, email, password: hashedPassword});
-            return result;
-    }
+  @EventPattern('test')
+  async hello() {
+    console.log('  ');
+  }
 
-    @Get(':id')
-    async get(@Param('id') id: number){
-        console.log("id: ", id);
-        const result = this.client.send('user_request_single', id);
-        return result;
-        // return this.userService.get(id);
-    }
+  @EventPattern('product_created')
+  async helloNew(product: any) {
+    console.log(product);
+  }
 
-    @Put(':id')
-    async update(
-        @Param('id') id: number,
-        @Body('firstName') firstName: string,
-        @Body('lastName') lastName: string,
-        @Body('email') email: string,
-        @Body('password') password: string,
-    ){
-        const result = this.client.send('user_updated_gateway', {id, firstName, lastName, email, password});
-        return result;
-        // return this.userService.update(id, {
-        //     firstName,
-        //     lastName,
-        //     email,
-        //     password
-        // });
-    }
+  @Post('register')
+  async register(
+    @Body('firstName') firstName: string,
+    @Body('lastName') lastName: string,
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    const hashedPassword = await bcrypt.hash(password, 12);
+    console.log('register');
 
-    @Delete(':id')
-    async delete(@Param('id') id: number) {
-        const result = this.client.send('user_deleted_gateway', {id});
-        return result;
-        // return this.userService.delete(id);
-    }
+    const result = this.client.send('user_created_gateway', { firstName, lastName, email, password: hashedPassword });
+    return result;
+  }
+
+  @Get(':id')
+  async get(@Param('id') id: number) {
+    console.log('id: ', id);
+    const result = this.client.send('user_request_single', id);
+    return result;
+    // return this.userService.get(id);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: number,
+    @Body('firstName') firstName: string,
+    @Body('lastName') lastName: string,
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    const result = this.client.send('user_updated_gateway', { id, firstName, lastName, email, password });
+    return result;
+    // return this.userService.update(id, {
+    //     firstName,
+    //     lastName,
+    //     email,
+    //     password
+    // });
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number) {
+    const result = this.client.send('user_deleted_gateway', { id });
+    return result;
+    // return this.userService.delete(id);
+  }
 }
